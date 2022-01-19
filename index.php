ee6f54ab8b38ba46dffdd850fc3d7633c9c37fdb
unknown <kd1300747@kobedenshi078.onmicrosoft.com>
2022-01-19 18:23:41 +0900

    A

--- index.php
+++ index.php
@@ -1,1 +1,59 @@
 <?php ?>
+<!DOCTYPE html>
+<html lang="jp">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=
+  , initial-scale=1.0">
+  <title>Document</title>
+    <link rel="stylesheet" href="./shrinky.css">
+    <title>しゅりんきーくえすと</title>
+  </head>
+  <body>
+    <div style="text-align:center">
+      <p><br><br></p>
+      <img src="しゅりんきーくえすと.png">
+      <p><br><br></p>
+      <a href="#" class="btn-circle-3d">ぷれい</a>
+      <p><br><br></p>
+      <h5>MENU</h5>
+        <a href="#one">しゅりんきーくえすとってなに？</a>
+        <h3 id="one">しゅりんきーくえすとってなに？</h3>
+    </div>
+
+    <div style="text-align:center">
+      <div class="balloon2">
+        <p>ITやコロナについてのクイズができるよ！<br>
+          何問正解できるかな？<br></p>
+      </div>
+    </div>
+
+    <div style="text-align:center">
+      <img src="neko.png"width="150">
+      <p><br><br></p>
+    </div>
+
+    <div style="text-align:center">
+      <h3>すたーとがめん　　　　　　　　　　　　　　　　　　　　　ぷれいがめん</h3>
+      <tb><img src="title.png"width="500"> <img src="play.png"width="500"><tb>
+      <p><br></p>
+      <div class="balloon2">
+        <p>10問ごとになっているからさくさく楽しく<br>
+          問題がとけるよ！<br></p>
+      </div>
+    </div>
+
+    <div style="text-align:center">
+      <img src="neko.png"width="150">
+
+    <h3>作成者</h3>
+    <p>企画／富山日那多・宿野菜々子<br>
+    プログラマー（代表）／富山日那多<br>
+    発表者／花岡柚奈<br>
+    発表補助／宿野菜々子・中村志穂<br>
+    発表資料・動画作成／花岡柚奈・宿野菜々子<br>
+    HP作成／宮本玲奈・宮本憩・中村志穂<br>
+  AOポスター／矢内久留美・花岡柚奈</p>
+  </body>
+</html>
